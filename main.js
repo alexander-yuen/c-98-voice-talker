0918b17080ace429dcd16892391969b8f0c50185
alexander-yuen <79123774+alexander-yuen@users.noreply.github.com>
2021-08-16 20:29:57 -0400

    save , snap_shot, time function

--- main.js
+++ main.js
@@ -13,17 +13,30 @@ recognition.onresult = function run (event) {
     console.log(Content);
 
     document.getElementById("textbox").innerHTML = Content;
-speak();
+
+if(Content =="take my selfie"){
+    console.log("taking selfie --- ");
+
+    speak();
+}
 }
 
 function speak(){
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking your Selfie in 5 seconds";
+
+    
+    // document.getElementById("textbox").value;
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000);
 }
 
 Webcam.set({
@@ -33,3 +46,16 @@ Webcam.set({
     png_quality
 });
 camera = document.getElementById("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
